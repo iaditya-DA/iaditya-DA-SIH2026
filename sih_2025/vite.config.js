a43ae1a4ad7fa9a05d4e494fc45feaad7ae1d4f8
imcoolthanyou <jaiswanigautam@gmail.com>
2025-08-29 18:34:44 +0530

    "did"

--- sih_2025/vite.config.js
+++ sih_2025/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),tailwindcss(),
-    tsconfigPaths({
-      projects: ["./jsconfig.json"], // 👈 force it to use jsconfig
-    }),
+   
   ],
 });
