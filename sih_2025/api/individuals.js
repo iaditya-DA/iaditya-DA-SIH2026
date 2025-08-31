6ed93b1292537f2f699982b2a0707e2ffd7df473
imcoolthanyou <jaiswanigautam@gmail.com>
2025-08-31 16:28:40 +0530

    "fixed import issue"

--- sih_2025/api/individuals.js
+++ sih_2025/api/individuals.js
@@ -1,5 +1,5 @@
 import connectDB from '../lib/mongo.js';
-import { Individual } from '../../Models/SIHSCHEMA.js';
+import { Individual } from './Models/SIHSCHEMA.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
--- sih_2025/api/register/team.js
+++ sih_2025/api/register/team.js
@@ -1,5 +1,5 @@
 import connectDB from '../../lib/mongo.js';
-import { Team } from '../../Models/SIHSCHEMA.js';
+import { Team } from '../Models/SIHSCHEMA.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
